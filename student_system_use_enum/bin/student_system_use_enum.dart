1d4d1dcbc1abcb7bcead5a3f0678569dd93a7637
Mayasa164 <139007766+Mayasa164@users.noreply.github.com>
2023-08-30 12:35:42 +0400

    student_system_use_enum

---
+++ student_system_use_enum/bin/student_system_use_enum.dart
@@ -0,0 +1,23 @@
+void main(){
+Map stu1 = {
+MapKeys.name.name:"Mayasa",
+MapKeys.email.name:"mmm@gmail.com",
+MapKeys.phone.name:9887,
+MapKeys.address.name:"S"
+
+
+
+
+};
+
+print(stu1);
+
+}
+
+enum MapKeys{
+name,
+email,
+phone,
+address
+
+}
---
+++ student_system_use_enum/test/student_system_use_enum_test.dart
@@ -0,0 +1,8 @@
+import 'package:student_system_use_enum/student_system_use_enum.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
